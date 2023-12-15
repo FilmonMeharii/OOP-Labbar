a0edf877a00f4df1d97cfd7b6ac5bdbff33ba16f
nomliF <gefi21gd@student.ju.se>
2023-12-15 10:35:58 +0100

    Har implementerat korthand
    Har implementerat korthand h och cpp filerna

--- Kortspel21/korthand.cpp
+++ Kortspel21/korthand.cpp
@@ -4,3 +4,48 @@ Korthand::Korthand()
 {
 
 }
+
+void Korthand::rensa()
+{
+    m_korten.clear();
+}
+
+int Korthand::antalKort() const
+{
+    return m_korten.size();
+}
+
+Kort Korthand::kort(int nr) const
+{
+    return m_korten[nr];
+}
+
+void Korthand::stoppaInKort(const Kort &kort)
+{
+   m_korten.push_back(kort);
+}
+
+int Korthand::poang() const
+{
+    int totalPoang = 0;
+    int numEss =0;
+    for(const Kort &k:m_korten){
+        int kortVarde = k.valor();
+        if(kortVarde == 1){
+            numEss++;
+            if(totalPoang + 14 <= 21){
+                numEss +=14;
+            }else{
+                totalPoang +=1;
+            }
+        }else{
+            totalPoang +=kortVarde;
+        }
+    }
+    while (numEss > 1 && totalPoang>21) {
+        totalPoang -=13;
+        numEss -=1;
+    }
+    return totalPoang;
+
+}
--- Kortspel21/main.cpp
+++ Kortspel21/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include "korthand.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Hello World!" << endl;
-
+    Korthand hand;
     return 0;
 }
